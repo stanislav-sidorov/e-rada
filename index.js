4ef81d3f5ba68610f1fc09d7884b36a8bcb2ec7c
unknown <staskmp@gmail.com>
2018-12-28 01:58:44 +0200

    localhost running index.js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function hello() {
 console.log(`Hello node.js!\nUsing ${process.version} node version.`);
 }
 hello();
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
