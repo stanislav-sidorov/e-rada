3a1b5cf40cce88d7315f2b567cca92daf3362d79
unknown <staskmp@gmail.com>
2018-12-28 12:16:17 +0200

    pick up from e-rada

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ hello();
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
+  res.write('Done');
   res.end();
-}).listen(8080);
+}).listen(8000);
+
+ 
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://data.rada.gov.ua/ogd/etc/global_stat.json', true);
+
+request.onload = function () {
+	var data = JSON.parse(this.response);
+	for (var i = 0; i < data.length; i++) {
+		console.log(data[i].ids);
+	}
+}
+
+request.send();
